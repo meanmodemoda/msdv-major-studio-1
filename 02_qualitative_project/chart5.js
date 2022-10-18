b13ffedb1f272a437ae8597d5ba599839e11078f
Yang Zhao <freda.y.zhao@gmail.com>
2022-10-17 21:48:36 -0400

    update

--- 02_qualitative_project/chart5.js
+++ 02_qualitative_project/chart5.js
@@ -54,12 +54,11 @@ async function drawChart() {
   //4. Draw Scale
   const dataCircle = root.descendants();
   const circleScale = d3
-    .scaleRadial()
+    .scaleLinear()
     .domain(d3.extent(dataCircle, (d) => d.height))
-    .range([1, 7]);
+    .range([1, 20]);
 
   //Draw Tree
-
   const linksGenerator = d3
     .linkRadial()
     .angle(function (d) {
@@ -78,6 +77,24 @@ async function drawChart() {
     .style("fill", "none")
     .attr("stroke", "#ccc");
 
+  //
+  const defs = bounds.append("svg:defs");
+  defs
+    .append("svg:pattern")
+    .attr("id", "image1")
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("patternUnits", "useSpaceOnUse")
+    .append("svg:image")
+    .attr(
+      "xlink:href",
+      "https://raw.githubusercontent.com/muonius/msdv-major-studio-1/54b45a8dedcdd9f3662fa887e4f86b6808f5014e/02_qualitative_project/assets/1.png"
+    )
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("x", 0)
+    .attr("y", 0);
+  // .attr("transform", "rotate(90)");
   // Add a circle for each node.
   bounds
     .selectAll("g")
@@ -89,10 +106,11 @@ async function drawChart() {
     })
     .append("circle")
     .attr("r", (d) => circleScale(d.height))
-    .style("fill", "url(#img1)")
+    .style("fill", "url(#image1)")
     .attr("stroke", "black")
     .style("stroke-width", 0.5);
 
   console.log(d3.extent(dataCircle, (d) => d.height));
+  console.log(root.children[0].children[0].data);
 }
 drawChart();
