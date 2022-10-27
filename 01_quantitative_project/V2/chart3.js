8d24b31fd98c8991b1a0acb4e9b18abfa3ea2d97
Yang Zhao <freda.y.zhao@gmail.com>
2022-10-26 20:49:06 -0400

    bar

--- 01_quantitative_project/V2/chart3.js
+++ 01_quantitative_project/V2/chart3.js
@@ -1,14 +1,15 @@
 // set the dimensions and margins of the graph
 const margin = { top: 30, right: 0, bottom: 30, left: 50 },
-  width = 210 - margin.left - margin.right,
+  width = 810 - margin.left - margin.right,
   height = 210 - margin.top - margin.bottom;
 
 //Read the data
 d3.csv("./data/score.csv").then(function (data) {
   // group the data: I want to draw one line per group
   const sumstat = d3.group(data, (d) => d.region); // nest function allows to group the calculation per level of a factor
+  const goal = d3.groups(data, (d) => d.goal).map((d) => d[0]);
 
-  console.log(sumstat);
+  console.log(goal);
   // What is the list of groups?
   allKeys = new Set(data.map((d) => d.region));
 
@@ -25,20 +26,18 @@ d3.csv("./data/score.csv").then(function (data) {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
+  console.log(
+    d3.extent(data, function (d) {
+      return d.goal;
+    })
+  );
+
   // Add X axis --> it is a date format
-  const x = d3
-    .scaleBand()
-    .domain(
-      d3.extent(data, function (d) {
-        return d.goal;
-      })
-    )
-    .range([0, width])
-    .padding(0.02);
+  const x = d3.scaleBand().domain(goal).range([0, width]).padding(0.02);
   svg
     .append("g")
     .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).ticks(3));
+    .call(d3.axisBottom(x).ticks(17));
 
   //Add Y axis
   const y = d3
@@ -55,7 +54,7 @@ d3.csv("./data/score.csv").then(function (data) {
   // color palette
   const color = d3
     .scaleOrdinal()
-    //.domain(allKeys)
+    .domain(allKeys)
     .range([
       "#e41a1c",
       "#377eb8",
@@ -66,6 +65,29 @@ d3.csv("./data/score.csv").then(function (data) {
       "#a65628",
       "#f781bf",
       "#999999",
+    ]);
+
+  const color2 = d3
+    .scaleOrdinal()
+    .domain(goal)
+    .range([
+      "#F4D6E6",
+      "#FFEACF",
+      "#D3E8D9",
+      "#C0E4ED",
+      "#F58B94",
+      "#C78B9B",
+      "#84BCE0",
+      "#F4D6E6",
+      "#FFEACF",
+      "#D3E8D9",
+      "#C0E4ED",
+      "#F58B94",
+      "#C78B9B",
+      "#84BCE0",
+      "#F58B94",
+      "#C78B9B",
+      "#84BCE0",
     ]);
 
   // Draw the line
@@ -86,7 +108,7 @@ d3.csv("./data/score.csv").then(function (data) {
     .attr("height", function (d) {
       return height - y(d.value);
     })
-    .attr("fill", "#69b3a2");
+    .attr("fill", (d) => color2(d.goal));
 
   // .attr("d", function (d) {
   //   return d3
