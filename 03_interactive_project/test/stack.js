7f3e4aef3fa9d5f73d0ca5308d1dff779fe645f2
Yang Zhao <freda.y.zhao@gmail.com>
2022-11-26 10:11:35 -0500

    calculator

--- 03_interactive_project/test/stack.js
+++ 03_interactive_project/test/stack.js
@@ -1,114 +1,69 @@
-const margin = { top: 10, right: 30, bottom: 20, left: 50 },
-  width2 = 200 - margin.left - margin.right,
-  height2 = 150 - margin.top - margin.bottom;
+function drawStack() {
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 },
+    width2 = 150 - margin.left - margin.right,
+    height2 = 150 - margin.top - margin.bottom,
+    category2 = [
+      "Other",
+      "Personal care",
+      "Paid work or study",
+      "Unpaid care work",
+      "Leisure",
+    ];
 
-// append the svg object to the body of the page
-const svg2 = d3
-  .select("#stack")
-  .append("svg")
-  .attr("width", width2 + margin.left + margin.right)
-  .attr("height", height2 + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  let xStackScale, yStackScale, colorStackScale, stackGenerator;
 
-category2 = [
-  "Other",
-  "Personal care",
-  "Paid work or study",
-  "Unpaid care work",
-  "Leisure",
-];
+  d3.csv("../data/country.csv").then(function (data) {
+    // prepare data and stack generator
+    const sumstat = d3.groups(data, (d) => d.Country);
+    stackGenerator = d3.stack().keys(category2);
+    initializeLayout();
+    sumstat.forEach((d) => {
+      drawData(d);
+    });
+  });
 
-d3.csv("../data/country3.csv").then(function (data) {
-  var color2 = d3
-    .scaleOrdinal()
-    .domain(category2)
-    .range(["#FEF793", "#F47F33", "#92C5E1", "#FBBE85", "#AFBAFC"]);
-  //.range(["#3C1900","#DDC0B4","#C99F88","#B17E65","#A16A54","#855137","#5F3310"])
+  function drawData(d) {
+    const stackedData = stackGenerator(d[1]);
+    // console.log(stackedData);
+    const svg = d3
+      .select("#stack")
+      .append("svg")
+      .attr("clases", d[0])
+      .attr("width", width2 + margin.left + margin.right)
+      .attr("height", height2 + margin.top + margin.bottom)
+      // .attr("viewBox", "0 0 160 160")
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
+      // .attr("transform", `translate(10) rotate(45)`)
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .join("g")
+      .attr("fill", (d) => colorStackScale(d.key))
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data((d) => d)
+      .join("rect")
+      .attr("class", "watercolor")
+      .attr("x", (d) => xStackScale(d.data.Gender))
+      .attr("y", (d) => yStackScale(d[1]))
+      .attr("height", (d) => yStackScale(d[0]) - yStackScale(d[1]))
+      .attr("width", xStackScale.bandwidth());
+  }
 
-  // const pivot = data.map((d) => {
-  //   let obj = {};
-  //   obj.country = d.country;
-  //   obj.gender = d.gender;
-  //   obj[d.category] = d.value;
-  //   return obj;
-  // });
+  function initializeLayout() {
+    colorStackScale = d3
+      .scaleOrdinal()
+      .domain(category2)
+      .range(["#FEF793", "#F47F33", "#92C5E1", "#FBBE85", "#AFBAFC"]);
 
-  console.log(data);
+    xStackScale = d3
+      .scaleBand()
+      .domain(["Men", "Women"])
+      .range([0, width2])
+      .padding([0.05]);
 
-  const filtered = data.filter((d) => d.country == "Mexico");
-
-  const xScale = d3
-    .scaleBand()
-    .domain(["Men", "Women"])
-    .range([0, width2])
-    .padding([0.05]);
-
-  const yScale = d3.scaleLinear().domain([0, 1]).range([height2, 0]);
-  const stackedGen = d3.stack().keys(category2);
-  const stackedData = stackedGen(data);
-  console.log(stackedData[0]);
-
-  svg2
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-
-    .join("g")
-    .attr("fill", (d) => color2(d.key))
-    .selectAll("rect")
-    // enter a second time = loop subgroup per subgroup to add all rectangles
-    .data((d) => d)
-    .join("rect")
-    .attr("class", "men")
-    .attr("x", (d) => xScale(d.data["Gender"]))
-    .attr("y", (d) => yScale(d[1]))
-    .attr("height", (d) => yScale(d[0]) - yScale(d[1]))
-    .attr("width", xScale.bandwidth());
-});
-
-// // .attr("y", function (d, i) {
-// //   row = i / rows;
-// //   return rows * squareSize - (row * squareSize + row * gap);
-// // });
-// // // .append("title")
-// // //   .text(function (d,i)
-// // //     {
-// // //       return "Age range: " + data[d.groupIndex].age + " | " +  d.count + " , " + d.units + "%"
-// // //     });
-
-// // //add legend with categorical data
-// // // var legend = d3
-// // //   .select("#legend1")
-// // //   .append("svg")
-// // //   .attr("width", 100)
-// // //   .attr("height", 150)
-// // //   .append("g")
-// // //   .selectAll("div")
-// // //   .data(data)
-// // //   .enter()
-// // //   .append("g")
-// // //   .attr("transform", function (d, i) {
-// // //     return "translate(0," + i * 20 + ")";
-// // //   });
-// // // legend
-// // //   .append("rect")
-// // //   .attr("width", 18)
-// // //   .attr("height", 18)
-// // //   .style("fill", function (d, i) {
-// // //     return color(i);
-// // //   });
-// // // legend
-// // //   .append("text")
-// // //   .attr("x", 25)
-// // //   .attr("y", 13)
-// // //   .text(function (d) {
-// // //     return d.gender;
-// // //   });
-
-// // //add value of a unit square
-// // var legend2 = d3
-// //   .select("#legend")
-// //   .select("svg")
-// //   .append("g")
-// //   .attr("transform", "translate(100,0)");
+    yStackScale = d3.scaleLinear().domain([0, 1]).range([height2, 0]);
+  }
+}
+drawStack();
--- 03_interactive_project/test/waffle.js
+++ 03_interactive_project/test/waffle.js
@@ -1,138 +1,155 @@
-var total = 0;
-var columns = 52,
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+let summary = document.querySelector("#summary");
+summary.innerHTML = `<p>On average, at the age of <span>18</span>, women have 1 weeks less leisure time than men.`;
+
+let comparison = {
+  Men: { Leisure: 13, "Unpaid care work": 10, "Paid work or study": 4 },
+  Women: { Leisure: 12, "Unpaid care work": 10, "Paid work or study": 4 },
+};
+
+let diff = {
+  Leisure: 0,
+  "Unpaid care work": 0,
+  "Paid work or study": 0,
+};
+
+let leisureDiff, unpaidDiff, paidDiff;
+
+//Section 2: Create Waffle Chart
+let columns = 52,
   rows = 48,
   squareSize = 8,
   squareValue = 1,
   gap = 2,
-  theData = [];
-category = [
-  "Leisure",
-  "Unpaid work",
-  "Paid work or study",
-  "Personal care",
-  "Other",
-];
+  total = rows * columns,
+  category = [
+    "Leisure",
+    "Unpaid care work",
+    "Paid work or study",
+    "Personal care",
+    "Other",
+  ],
+  width = squareSize * columns + columns * gap + 25,
+  height = squareSize * rows + rows * gap + 25;
+
+let waffleData = [],
+  uniqueAge,
+  colorWaffleScale,
+  yWaffleScale;
 
 d3.csv("../data/age.csv").then(function (data) {
-  var color = d3
-    .scaleOrdinal()
-    .domain(category)
-    .range(["#AFBAFC", "#FBBE85", "#92C5E1", "#F47F33", "#FEF793"]);
-  //.range(["#3C1900","#DDC0B4","#C99F88","#B17E65","#A16A54","#855137","#5F3310"])
-  const data1 = d3.groups(data, (d) => d.gender);
+  /// Section 1: Create Slider
 
-  data1.forEach((data) => {
-    sumstat = d3.groups(data[1], (d) => d.age);
-    //total
-    total = rows * columns;
-    //value of a square
-    sumstat.forEach((age) => {
-      // //remap data
-      age[1].forEach(function (d, i) {
-        d.week = +d.week;
-        d.units = Math.floor(d.week / squareValue);
-        theData = theData.concat(
-          Array(d.units + 1)
-            .join(1)
-            .split("")
-            .map(function () {
-              return {
-                age: +age[0],
-                squareValue: squareValue,
-                units: d.units,
-                category: d.category,
-              };
-            })
-        );
-      });
-    });
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+    if (this.value > 18) {
+      calculator(data, this.value, comparison);
+    } else {
+      summary.innerHTML = `<p>On average, at the age of <span>18</span>, women have 1 weeks less leisure time than men.`;
+    }
+  };
 
-    width = squareSize * columns + columns * gap + 25;
-    height = squareSize * rows + rows * gap + 25;
+  const dataGender = d3.groups(data, (d) => d.gender);
 
-    const uniqueAge = theData
-      .map((d) => d.age)
-      .reduce((acc, curr) => {
-        if (!acc.includes(curr)) {
-          acc.push(curr);
-        }
-        return acc;
-      }, []);
-
-    const yScale = d3
-      .scaleBand()
-      .range([0, height])
-      .domain(uniqueAge)
-      .padding(0.1);
-
-    var waffle = d3
-      .select("#waffle1")
-      .append("svg")
-      .attr("class", data[0].toLowerCase())
-      .attr("width", width)
-      .attr("height", height);
-
-    waffle
-      .append("g")
-      .selectAll("rect")
-      .data(theData)
-      .join("rect")
-      .attr("width", squareSize)
-      .attr("height", squareSize)
-      .attr("fill", function (d) {
-        return color(d.category);
-      })
-      .attr("x", function (d, i) {
-        //group n squares for column
-        colIndex = Math.floor(i % columns);
-        return colIndex * squareSize + colIndex * gap;
-      })
-      .attr("y", (d) => yScale(d.age));
+  dataGender.forEach((data) => {
+    prepareData(data);
+    initializeLayout();
+    drawWaffle(data);
   });
 });
 
-// // .attr("y", function (d, i) {
-// //   row = i / rows;
-// //   return rows * squareSize - (row * squareSize + row * gap);
-// // });
-// // // .append("title")
-// // //   .text(function (d,i)
-// // //     {
-// // //       return "Age range: " + data[d.groupIndex].age + " | " +  d.count + " , " + d.units + "%"
-// // //     });
+function drawWaffle(data) {
+  let waffle = d3
+    .select("#waffle")
+    .append("svg")
+    .attr("id", data[0].toLowerCase())
+    .attr("class", "watercolor")
+    .attr("width", width)
+    .attr("height", height);
 
-// // //add legend with categorical data
-// // // var legend = d3
-// // //   .select("#legend1")
-// // //   .append("svg")
-// // //   .attr("width", 100)
-// // //   .attr("height", 150)
-// // //   .append("g")
-// // //   .selectAll("div")
-// // //   .data(data)
-// // //   .enter()
-// // //   .append("g")
-// // //   .attr("transform", function (d, i) {
-// // //     return "translate(0," + i * 20 + ")";
-// // //   });
-// // // legend
-// // //   .append("rect")
-// // //   .attr("width", 18)
-// // //   .attr("height", 18)
-// // //   .style("fill", function (d, i) {
-// // //     return color(i);
-// // //   });
-// // // legend
-// // //   .append("text")
-// // //   .attr("x", 25)
-// // //   .attr("y", 13)
-// // //   .text(function (d) {
-// // //     return d.gender;
-// // //   });
+  waffle
+    .append("g")
+    .selectAll("rect")
+    .data(waffleData)
+    .join("rect")
+    .attr("width", squareSize)
+    .attr("height", squareSize)
+    .attr("fill", (d) => colorWaffleScale(d.category))
+    .attr("x", function (d, i) {
+      //group n squares for column
+      colIndex = Math.floor(i % columns);
+      return colIndex * squareSize + colIndex * gap;
+    })
+    .attr("y", (d) => yWaffleScale(d.age));
+}
 
-// // //add value of a unit square
-// // var legend2 = d3
-// //   .select("#legend")
-// //   .select("svg")
-// //   .append("g")
-// //   .attr("transform", "translate(100,0)");
+function prepareData(data) {
+  //Preparere data
+  sumstat = d3.groups(data[1], (d) => d.age);
+  sumstat.forEach((age) => {
+    age[1].forEach(function (d, i) {
+      d.week = +d.week;
+      d.units = Math.floor(d.week / squareValue);
+      waffleData = waffleData.concat(
+        Array(d.units + 1)
+          .join(1)
+          .split("")
+          .map(function () {
+            return {
+              age: +age[0],
+              squareValue: squareValue,
+              units: d.units,
+              category: d.category,
+            };
+          })
+      );
+    });
+  });
+
+  // console.log(waffleData)
+  uniqueAge = waffleData
+    .map((d) => d.age)
+    .reduce((acc, curr) => {
+      if (!acc.includes(curr)) {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
+}
+
+function initializeLayout() {
+  //initalize Layout
+  yWaffleScale = d3
+    .scaleBand()
+    .range([0, height])
+    .domain(uniqueAge)
+    .padding(0.1);
+
+  colorWaffleScale = d3
+    .scaleOrdinal()
+    .domain(category)
+    .range(["#AFBAFC", "#FBBE85", "#92C5E1", "#F47F33", "#FEF793"]);
+}
+
+function calculator(data, sliderValue, comparison) {
+  const gender = Object.keys(comparison);
+  gender.forEach((g) => {
+    para = Object.keys(comparison[g]);
+    para.forEach((p) => {
+      comparison[g][p] = data
+        .filter((d) => d.gender == g && d.age <= sliderValue && d.category == p)
+        .reduce((acc, curr) => {
+          acc += Number(curr.week);
+          return acc;
+        }, 0);
+
+      diff[p] = Math.abs(comparison["Men"][p] - comparison["Women"][p]);
+    });
+  });
+
+  let summary = document.querySelector("#summary");
+  summary.innerHTML = `<p>By age of <span>${sliderValue}</span>,women have ${diff["Leisure"]} weeks less leisure time and ${diff["Unpaid care work"]} weeks more unpaid care time than men. They also have ${diff["Paid work or study"]} weeks less paid work or study.</p>`;
+}
